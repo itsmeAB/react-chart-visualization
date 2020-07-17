3bb121ff3155ae34cf1ee3277a29b7fd38d5f85a
itsmeAB <ss.abhilash3@gmail.com>
2020-07-18 03:10:14 +0530

    initialize scss

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './routes/home';
 // import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
